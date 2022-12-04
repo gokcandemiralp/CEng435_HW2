0b8e06888c1ff86ee367c8cec0ae92aaef9ae60a
Gökcan Demiralp <gok.can.13@gmail.com>
2022-12-05 00:54:14 +0300

    udp version

--- server.cpp
+++ server.cpp
@@ -1,112 +1,108 @@
 #include "common.h"
-#include <netinet/in.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <unistd.h>
+#include <arpa/inet.h>
 #include <cstdio>
 #include <pthread.h>
 #include <cstdlib>
-#define PORT 2222
+#include <netinet/in.h>
+#include <unistd.h>
 
+bool terminate = false;
 pthread_t listen_t;
 pthread_t send_t;
-size_t buffersize = 1024;
-char* buffer;
-bool terminate = false;
-bool new_sent_message = false;
+unsigned int packetCount = 0;
+unsigned int ackCount = 0;
 
-int server_fd, new_socket, valread;
-struct sockaddr_in address;
-int opt = 1;
-int addrlen = sizeof(address);
-char listen_buffer[1024] = { 0 };
-char* hello = "Hello from server";
+int socket_desc;
+struct sockaddr_in server_addr, client_addr;
+size_t buffersize = 100;
+char *server_packet;
+char *client_packet;
+socklen_t client_struct_length = sizeof(client_addr);
 
 int setSocket(){
- 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
- 
-    if (setsockopt(server_fd, SOL_SOCKET,
-                   SO_REUSEADDR | SO_REUSEPORT, &opt,
-                   sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    server_packet = (char*) std::malloc(buffersize*sizeof(char));
+     client_packet = (char*) std::malloc(buffersize*sizeof(char));
 
-    if (bind(server_fd, (struct sockaddr*)&address,sizeof(address))< 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    if ((new_socket= accept(server_fd, (struct sockaddr*)&address,(socklen_t*)&addrlen))< 0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
+    memset(server_packet, '\0', sizeof(server_packet));
+    memset( client_packet, '\0', sizeof( client_packet));
 
-    return 0;
+    socket_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    
+    if(socket_desc < 0){
+        printf("Error while creating socket\n");
+        return -1;
+    }
+    printf("Socket created successfully\n");
+    
+    // Set port and IP:
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(2000);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    
+    // Bind to the set port and IP:
+    if(bind(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
+        printf("Couldn't bind to the port\n");
+        return -1;
+    }
+    printf("Done with binding\n");
+    return 0;    
 }
 
 void* listen_routine(void* args){
+    int newLineCount = 0;
     while(!terminate){
-        valread = read(new_socket, listen_buffer, 1024);
-        printf("%s\n", listen_buffer);
+        if (recvfrom(socket_desc,  client_packet, sizeof( client_packet), 0,
+            (struct sockaddr*)&client_addr, &client_struct_length) < 0){
+            printf("Couldn't receive\n");
+            return nullptr;
+        }
+
+        if(!strcmp(client_packet,"\n")){
+            ++newLineCount;
+        }
+        else{
+            newLineCount = 0;
+            ++packetCount;
+        }
+        if(newLineCount == 3){ //termination condition
+            terminate = true;
+            close(socket_desc);
+            return 0;
+        }
+        
+        printf("Received message from IP: %s and port: %i\n",
+        inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        
     }
     return nullptr;
 }
 
 void* send_routine(void* args){
     while(!terminate){
-        if(new_sent_message){
-            new_sent_message = false;
-            send(new_socket, hello, strlen(hello), 0);
-            printf("Hello message sent\n");
+        if(packetCount>ackCount){
+            printf("Msg from client: %s\n",  client_packet);
+            strcpy(server_packet,  client_packet);
+            
+            if (sendto(socket_desc, server_packet, strlen(server_packet), 0,
+                (struct sockaddr*)&client_addr, client_struct_length) < 0){
+                printf("Can't send\n");
+                return nullptr;
+            }
+            ++ackCount;
+            printf("%d\n",ackCount);
         }
     }
     return nullptr;
 }
 
-int loop(){
-    int newLineCount = 0;
-    while(1){
-        getline(&buffer, &buffersize ,stdin);
-        if(!strcmp(buffer,"\n")){
-            ++newLineCount;
-        }
-        else{
-            newLineCount = 0;
-        }
-        if(newLineCount == 3){ //termination condition
-            terminate = true;
-            close(new_socket);
-            shutdown(server_fd, SHUT_RDWR);
-            return 0;
-        }
-        buffer[strlen(buffer)-1] = '\0';
-        new_sent_message = true;
-    }
-}
-
 int main(void){
-    buffer = (char*) std::malloc(buffersize*sizeof(char));
-
     setSocket();
     pthread_create(&listen_t, nullptr, &listen_routine, nullptr);
     pthread_create(&send_t, nullptr, &send_routine, nullptr);
 
-    loop();
-
     pthread_join(listen_t, nullptr);
     pthread_join(send_t, nullptr);
-    return 0;
 }
