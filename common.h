d7a243c0214733cf5451130d1f7c560292fef9e2
Gökcan Demiralp <gok.can.13@gmail.com>
2022-12-05 12:51:00 +0300

    minimized the structs

--- common.h
+++ common.h
@@ -1,11 +1,5 @@
 #pragma pack(push, 1)
-struct {
-  unsigned int id;
-  char reserved[12];
-} myAck;
-
-#pragma pack(push, 1)
-struct {
+struct myPacket{
   unsigned int id;
   char content[12];
-} myPacket;
+};
