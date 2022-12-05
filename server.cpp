7e9802961e62142869c23d467b3ea440c010dc57
Gökcan Demiralp <gok.can.13@gmail.com>
2022-12-05 16:53:26 +0300

    temination condition fixed

--- server.cpp
+++ server.cpp
@@ -9,9 +9,9 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
-bool terminate = false;
 pthread_t listen_t;
 pthread_t send_t;
+bool terminate = false;
 
 unsigned int packetCount = 0;
 unsigned int ackCount = 0;
@@ -30,19 +30,18 @@ int setSocket(){
     client_packet = (myPacket*) std::malloc(sizeof(myPacket));
 
     memset(server_packet, '\0', sizeof(server_packet));
-    memset( client_packet, '\0', sizeof( client_packet));
+    memset(client_packet, '\0', sizeof( client_packet));
 
-    socket_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    socket_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); // Generate the Socket
     
     if(socket_desc < 0){
         perror("Error while creating socket\n");
         exit(EXIT_FAILURE);
     }
     
-    // Set port and IP:
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port); //set the port
+    server_addr.sin_addr.s_addr = INADDR_ANY; //set the IP's
     
     // Bind to the set port and IP:
     if(bind(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
@@ -55,19 +54,14 @@ int setSocket(){
 void* listen_routine(void* args){
     int newLineCount = 0;
 
-    while(!terminate){
+    while(1){
         if (recvfrom(socket_desc,  client_packet, packetsize, 0,
             (struct sockaddr*)&client_addr, &client_struct_length) < 0){
             perror("Couldn't receive\n");
             exit(EXIT_FAILURE);
         }
-        if(packetCount = client_packet->id){
-            continue;
-        }
-        else{
-            packetCount = client_packet->id;
-        }
 
+        printf("%s",  client_packet->content);
         if(!strcmp(client_packet->content,"\n")){
             ++newLineCount;
         }
@@ -76,9 +70,9 @@ void* listen_routine(void* args){
             ++packetCount;
         }
         if(newLineCount == 3){ //termination condition
+            pthread_cancel(send_t);
             terminate = true;
-            close(socket_desc);
-            return 0;
+            return nullptr;
         }
     }
     return nullptr;
@@ -87,7 +81,6 @@ void* listen_routine(void* args){
 void* send_routine(void* args){
     while(!terminate){
         if(packetCount>ackCount){
-            printf("Msg from client: %s\n",  client_packet->content);
             server_packet = client_packet;
             
             if (sendto(socket_desc, server_packet, packetsize, 0,
